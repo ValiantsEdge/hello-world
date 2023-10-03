1008946c5e5875e986c6fce637295eb25d50667a
ValiantsEdge <petergallogly0@gmail.com>
2023-10-02 23:13:10 -0400

    second test commit (edits)

--- prac1.py
+++ prac1.py
@@ -2,11 +2,35 @@
 inp = input('What is your name?')
 print('Hello, '+inp)'''
 
-def convertToFahrenheit(degreesCelsius):
+'''def convertToFahrenheit(degreesCelsius):
     farh=degreesCelsius*(9/5)+32
     return farh
 def convertToCelsius(degressFarhenheit):
     cel=(degressFarhenheit-32)*(5/9)
     return cel
 assert convertToFahrenheit(0) == 32 
-assert convertToFahrenheit(100) == 212 
+assert convertToFahrenheit(100) == 212 '''
+
+'''def isOdd(number):
+    num = number%2
+    if num==1:
+        return True
+    else:
+        return False
+def isEven(number):
+    num = number%2
+    if num == 0:
+        return True
+    else:
+        return False
+assert isOdd(42) == False 
+assert isOdd(9999) == True
+assert isOdd(-10) == False 
+assert isOdd(-11) == True
+assert isOdd(3.1415) == False
+assert isEven(42) == True 
+assert isEven(9999) == False 
+assert isEven(-10) == True 
+assert isEven(-11) == False 
+assert isEven(3.1415) == False'''
+
