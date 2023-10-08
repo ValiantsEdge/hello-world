ca63944bdd575af0a9736953fea2b4d881883c03
ValiantsEdge <petergallogly0@gmail.com>
2023-10-08 17:33:17 -0400

    Functions

--- genquiz1.py
+++ genquiz1.py
@@ -1,5 +1,5 @@
 import random
-import tkinter as tk 
+import tkinter as tk
 from PIL import Image, ImageTk
 
 class pokemon():
@@ -9,43 +9,69 @@ class pokemon():
     
     def __str__(self):
         return self.name + " is from Gen " + self.gen
+
 #Dictionary for Pokemon and corresponding image and generations
 mons = {"Staraptor":("Projects/staraptor.png", "Gen 4"),
 "Regirock":("Projects/regirock.jpg", "Gen 3")}
 
-#Choosing a random mon from a list generated from the keys in our dictionary
-monchoice = random.choice(list(mons.keys()))
-#Choosing the monchoice value that corresponds with the randomly chosen key in the mons dictionary (in this case the value is a list), then chooses the respective value in the list
-monim=mons[monchoice][0]
-mongen=mons[monchoice][1]
+#Function for getting random mon and values
+def monGrabs():
+    global monchoice, monim, mongen
+    #Choosing a random mon from a list generated from the keys in our dictionary
+    monchoice = random.choice(list(mons.keys()))
+    #Choosing the monchoice value that corresponds with the randomly chosen key in the mons dictionary (in this case the value is a list), then chooses the respective value in the list
+    monim=mons[monchoice][0]
+    mongen=mons[monchoice][1]
+    return monchoice,monim,mongen
+monGrabs()
 
-
-# GUI 
-window =tk.Tk()
+#GUI
+window=tk.Tk()
 window.geometry("650x650")
 window.configure(bg="black")
 window.title("Gen Quiz")
+#number correct
+correct=0
 
-#Image
-starap = Image.open(monim)
-tkstarap = ImageTk.PhotoImage(starap)
-imglabel = tk.Label(window, image=tkstarap)
-imglabel.pack()
+def packImg(img1):
+    global monim
+    #Image
+    ppic = Image.open(monim)
+    ppic=ppic.resize((400,400))
+    tkppic = ImageTk.PhotoImage(ppic)
+    imglabel = tk.Label(window, image=tkppic)
+    imglabel.pack()
+packImg(monim)
 
 #Entry Field
 entry = tk.Entry(window)
 entry.pack()
 
-# Get the user input
+#Get the user input
 def userInput():
+    global correct
     inp = entry.get()
     if inp==mongen:
         print('Correct')
+        correct += 1
+        print(correct)
     else:
-        print('fail')
-    return(inp)
+        print('Fail')
+
+
+'''def runGame():
+    #GUI
+    global window
+    window=tk.Tk()
+    window.geometry("650x650")
+    window.configure(bg="black")
+    window.title("Gen Quiz")
+    #for i in range(0):
+    monGrabs()
+    packImg(monim)
+    userInput()'''
 
 getInput =tk.Button(window, text="Submit", command=userInput)
-getInput.pack()
+getInput.pack() 
 
 window.mainloop()
